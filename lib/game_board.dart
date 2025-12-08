583558aca5bec52627155acbd4988b43e516b8d2
IbrahimSenates <ibrahimsenates6@gmail.com>
2025-12-08 17:47:56 +0300

    taşların konumu

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,3 +1,7 @@
+import 'package:chess/components/piece.dart';
+import 'package:chess/components/square.dart';
+import 'package:chess/helper/helper_methods.dart';
+import 'package:chess/values/colors.dart';
 import 'package:flutter/material.dart';
 
 class GameBoard extends StatefulWidget {
@@ -8,16 +12,141 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
+  late List<List<ChessPiece?>> board;
+
+  @override
+  void initState() {
+    _initializeBoard();
+    super.initState();
+  }
+
+  void _initializeBoard() {
+    List<List<ChessPiece?>> newBoard = List.generate(
+      8,
+      (index) => List.generate(8, (index) => null),
+    );
+
+    //piyonların konumu
+    for (int i = 0; i < 8; i++) {
+      newBoard[1][i] = ChessPiece(
+        type: ChessPieceType.pawn,
+        isWhite: false,
+        imagePath: 'lib/images/pawn.png',
+      );
+      newBoard[6][i] = ChessPiece(
+        type: ChessPieceType.pawn,
+        isWhite: true,
+        imagePath: 'lib/images/pawn_white.png',
+      );
+
+      //kalelerin konumları
+      newBoard[0][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: 'lib/images/rook.png',
+      );
+      newBoard[0][7] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: false,
+        imagePath: 'lib/images/rook.png',
+      );
+      newBoard[7][0] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: true,
+        imagePath: 'lib/images/rook_white.png',
+      );
+      newBoard[7][7] = ChessPiece(
+        type: ChessPieceType.rook,
+        isWhite: true,
+        imagePath: 'lib/images/rook_white.png',
+      );
+
+      //atların konumları
+      newBoard[0][1] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: false,
+        imagePath: 'lib/images/knight.png',
+      );
+      newBoard[0][6] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: false,
+        imagePath: 'lib/images/knight.png',
+      );
+      newBoard[7][1] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: true,
+        imagePath: 'lib/images/knight_white.png',
+      );
+      newBoard[7][6] = ChessPiece(
+        type: ChessPieceType.knight,
+        isWhite: true,
+        imagePath: 'lib/images/knight_white.png',
+      );
+
+      //fillerin konumları
+      newBoard[0][2] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: false,
+        imagePath: 'lib/images/bishop.png',
+      );
+      newBoard[0][5] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: false,
+        imagePath: 'lib/images/bishop.png',
+      );
+      newBoard[7][2] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: true,
+        imagePath: 'lib/images/bishop_white.png',
+      );
+      newBoard[7][5] = ChessPiece(
+        type: ChessPieceType.bishop,
+        isWhite: true,
+        imagePath: 'lib/images/bishop_white.png',
+      );
+
+      //Vezirlerin konumu
+      newBoard[0][3] = ChessPiece(
+        type: ChessPieceType.queen,
+        isWhite: false,
+        imagePath: 'lib/images/queen.png',
+      );
+      newBoard[7][3] = ChessPiece(
+        type: ChessPieceType.queen,
+        isWhite: true,
+        imagePath: 'lib/images/queen_white.png',
+      );
+
+      //Şahların konumu
+      newBoard[0][4] = ChessPiece(
+        type: ChessPieceType.king,
+        isWhite: false,
+        imagePath: 'lib/images/king.png',
+      );
+      newBoard[7][4] = ChessPiece(
+        type: ChessPieceType.king,
+        isWhite: true,
+        imagePath: 'lib/images/king_white.png',
+      );
+    }
+    board = newBoard;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       body: GridView.builder(
         itemCount: 8 * 8,
         physics: NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 8,
         ),
-        itemBuilder: (context, index) => Text(index.toString()),
+        itemBuilder: (context, index) {
+          int row = index ~/ 8;
+          int col = index % 8;
+          return Square(isWhite: isWhite(index), piece: board[row][col]);
+        },
       ),
     );
   }
